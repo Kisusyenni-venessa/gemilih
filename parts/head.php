2a8f9aa1584906ebd938a883df6dfc1bd7c823ec
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-07-10 22:54:03 +0700

    Separate parts

---
+++ parts/head.php
@@ -0,0 +1,41 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Document Title -->
+    <title>Gemilih - Generasi Milenial Memilih)</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
+
+    <!--==== Google Fonts ====-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito%7CPlayfair+Display:400,700%7CYantramanav:400,700" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <!-- CSS Files -->
+
+    <!--==== Bootstrap css file ====-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--==== Font-Awesome css file ====-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!--==== Owl Carousel css file ====-->
+    <link rel="stylesheet" href="assets/plugins/owl-carousel/owl.carousel.min.css">
+
+    <!--==== Nice Select css file ====-->
+    <link rel="stylesheet" href="assets/plugins/nice-select/nice-select.css">
+
+    <!--==== Style css file ====-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--==== Responsive css file ====-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!--==== Custom css file ====-->
+    <link rel="stylesheet" href="assets/css/custom.css">
+    
+    <!--==== Modif css file ====-->
+    <link rel="stylesheet" href="assets/css/modif.css">
+</head>
---
+++ parts/loader.php
@@ -0,0 +1,10 @@
+<div class="preloader">
+    <div class="preload-img">
+        <div class="spinnerWave">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+        </div>
+    </div>
+</div>
