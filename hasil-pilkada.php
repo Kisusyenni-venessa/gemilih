0b350dda70868ee58a1d05612e6602a584581479
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-07-16 20:54:30 +0700

    Add blank content

--- hasil-pilkada.php
+++ hasil-pilkada.php
@@ -11,11 +11,258 @@
     <?php include_once("parts/header.php")?>
     <!-- End of Main Header -->
 
-    <!-- 
+    <!-- Page title -->
+    <div class="lawFirm--page-title text-center lawFirm--bg-center" data-bg-img="assets/images/lokasi-img/header-bg.png">
+        <div class="container">
+            <h2 class="pb-20">Lawyers</h2>
+            <div class="title-bc"> <a href="#">Home</a> > <span>Lawyers</span></div>
+        </div>
+    </div>
+    <!-- End of Page title -->
 
-    KONTEN MASUKIN SINI
+    <!-- Team -->
+    <div class="lawFirm--team pt-120 pb-70">
+        <div class="container">
+            <!-- Team Slider -->
+            <div class="lawFirm--team-cover">
+                <div class="row justify-content-sm-center">
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 01 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 01 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-1.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 01 Image -->
 
-     -->
+                            <!-- Item 01 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Andrea Anderson</a></h4>
+                                <p>Corporate Law</p>
+                            </div>
+                            <!-- End of Item 01 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 01 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 02 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 02 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-2.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 02 Image -->
+
+                            <!-- Item 02 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">James E. Hartley</a></h4>
+                                <p>Insurance Law</p>
+                            </div>
+                            <!-- End of Item 02 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 02 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 03 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 03 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-3.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 03 Image -->
+
+                            <!-- Item 03 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Kyle Gray</a></h4>
+                                <p>Environment Law</p>
+                            </div>
+                            <!-- End of Item 03 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 03 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 04 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 04 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-4.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 04 Image -->
+
+                            <!-- Item 04 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Laura S. Hundley</a></h4>
+                                <p>Corporate Law</p>
+                            </div>
+                            <!-- End of Item 04 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 04 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 05 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 05 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-5.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 05 Image -->
+
+                            <!-- Item 05 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Adrian Cartwright</a></h4>
+                                <p>Business Law</p>
+                            </div>
+                            <!-- End of Item 05 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 05 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 06 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 06 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-6.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 06 Image -->
+
+                            <!-- Item 06 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Mike Taylor</a></h4>
+                                <p>Corporate Law</p>
+                            </div>
+                            <!-- End of Item 06 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 06 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 07 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 07 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-7.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 07 Image -->
+
+                            <!-- Item 07 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Robert Leeb</a></h4>
+                                <p>Real State Law</p>
+                            </div>
+                            <!-- End of Item 07 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 07 -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 lawFirm--nth">
+                        <!-- Team Slider Item 08 -->
+                        <div class="lawFirm--team-slider-item">
+                            <!-- Item 08 Image -->
+                            <div class="lawFirm--team-slider-item-image">
+                                <!-- Image -->
+                                <img src="assets/images/team-images/person-8.jpg" alt="" class="img-fluid" data-rjs="2">
+                                <!-- End of Image -->
+                                <!-- Social Links-->
+                                <div class="social-links">
+                                    <a href=""><i class="fa fa-facebook-f"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-youtube"></i></a>
+                                </div>
+                                <!-- End of Social Links-->
+                            </div>
+                            <!-- End of Item 08 Image -->
+
+                            <!-- Item 08 Text-->
+                            <div class="lawFirm--team-slider-item-text text-center pt-25 pb-30">
+                                <h4> <a href="lawyer-details.html">Aline Cardin</a></h4>
+                                <p>Family Law</p>
+                            </div>
+                            <!-- End of Item 08 Text -->
+                        </div>
+                        <!-- End of Team Slider Item 08 -->
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Team -->
 
 
     <!-- Footer -->
