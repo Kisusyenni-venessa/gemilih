c612cd42a18d6d24943f861dfeba60fbf645b1a6
Steven Lim <stevenlim2306@gmail.com>
2020-07-17 00:24:47 +0700

    update

--- about.php
+++ about.php
@@ -14,11 +14,17 @@
     <!-- Page title -->
     <div class="lawFirm--page-title text-center lawFirm--bg-center" data-bg-img="assets/images/page-title-bg.png">
         <div class="container">
-            <h2 class="pb-20">Contact Us</h2>
-            <div class="title-bc"> <a href="#">Home</a> > <span>Contact Us </span></div>
+            <h2 class="pb-20">About Gemilih</h2>
+            <div class="title-bc"> <a href="index.php">Home</a> > <span>About Gemilih </span></div>
         </div>
     </div>
     <!-- End of Page title -->
+
+    <!-- Form -->
+    <div class="lawFirm--contact pt-120 pb-120 lawFirm--right-back-circle">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2Gg1zazzqOu_yYjzzUwA_4l94lf4-Fwo_Rkz2HrpgFmx0Hw/viewform?embedded=true" width="1903" height="801" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+    <!-- End Form -->
 
      <!-- Team -->
      <div class="lawFirm--team pt-120 pb-70 lawFirm--left-back-circle">
@@ -252,7 +258,7 @@
     <!-- End of Team -->
 
     <!-- Contact -->
-    <div class="lawFirm--contact pt-120 pb-120 lawFirm--right-back-circle">
+    <!-- <div class="lawFirm--contact pt-120 pb-120 lawFirm--right-back-circle">
         <div class="container">
             <div class="lawFirm--section-title text-left">
                 <h2> Let Us Know What’s Your <br> Problem <span></span></h2>
@@ -272,11 +278,11 @@
                                 <a class="nav-link" id="chicago-area" data-toggle="tab" href="#chicago" aria-expanded="false" aria-controls="chicago">Chicago</a>
                             </div>
                         </li>
-                    </ul>
+                    </ul> -->
 
-                    <div class="tab-content" id="myTabContent">
+                    <!-- <div class="tab-content" id="myTabContent"> -->
                         <!-- New York -->
-                        <div class="tab-pane fade show active" id="new-york" role="tabpanel" aria-labelledby="new-york-area">
+                        <!-- <div class="tab-pane fade show active" id="new-york" role="tabpanel" aria-labelledby="new-york-area">
                             <ul class="lawFirm--conact-options">
                                 <li class="mb-10">
                                     <i class="fa fa-map-marker"></i> 
@@ -293,23 +299,23 @@
                                     <strong> Email </strong> 
                                     <p>youremail@mailcom  &  info@gmail.com</p>
                                 </li>
-                            </ul>
+                            </ul> -->
                             <!-- End of Contact Options -->
 
                             <!-- Social Links -->
-                            <div class="lawFirm--contact-social-links">
+                            <!-- <div class="lawFirm--contact-social-links">
                                 <span><i class="fa fa-globe"></i> Follow us </span>
                                 <a href="#"><i class="fa fa-facebook"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                                 <a href="#"><i class="fa fa-youtube"></i></a>
-                            </div>
+                            </div> -->
                             <!-- End of Social Links -->
-                        </div>
+                        <!-- </div> -->
                         <!-- End of New York -->
 
                         <!-- Hong Kong -->
-                        <div class="tab-pane" id="hong-kong" role="tabpanel" aria-labelledby="hong-kong-area">
+                        <!-- <div class="tab-pane" id="hong-kong" role="tabpanel" aria-labelledby="hong-kong-area">
                             <ul class="lawFirm--conact-options">
                                 <li class="mb-10">
                                     <i class="fa fa-map-marker"></i> 
@@ -326,25 +332,25 @@
                                     <strong> Email </strong> 
                                     <p>youremail@mailcom  &  info@gmail.com</p>
                                 </li>
-                            </ul>
+                            </ul> -->
                             <!-- End of Contact Options -->
 
                             <!-- Social Links -->
-                            <div class="lawFirm--contact-social-links">
+                            <!-- <div class="lawFirm--contact-social-links">
                                 <span><i class="fa fa-globe"></i> Follow us </span>
                                 <a href="#"><i class="fa fa-facebook"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                                 <a href="#"><i class="fa fa-youtube"></i></a>
-                            </div>
+                            </div> -->
                             <!-- End of Social Links -->
-                        </div>
+                        <!-- </div> -->
                         <!-- End of Hong Kong -->
 
                         <!-- Chicago -->
-                        <div class="tab-pane" id="chicago" role="tabpanel" aria-labelledby="chicago-area">
+                        <!-- <div class="tab-pane" id="chicago" role="tabpanel" aria-labelledby="chicago-area"> -->
                             <!-- Contact Options -->
-                            <ul class="lawFirm--conact-options">
+                            <!-- <ul class="lawFirm--conact-options">
                                 <li class="mb-10">
                                     <i class="fa fa-map-marker"></i> 
                                     <strong> Address </strong>
@@ -360,61 +366,61 @@
                                     <strong> Email </strong> 
                                     <p>youremail@mailcom  &  info@gmail.com</p>
                                 </li>
-                            </ul>
+                            </ul> -->
                             <!-- End of Contact Options -->
 
                             <!-- Social Links -->
-                            <div class="lawFirm--contact-social-links">
+                            <!-- <div class="lawFirm--contact-social-links">
                                 <span><i class="fa fa-globe"></i> Follow us </span>
                                 <a href="#"><i class="fa fa-facebook"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                                 <a href="#"><i class="fa fa-youtube"></i></a>
-                            </div>
+                            </div> -->
                             <!-- End of Social Links -->
-                        </div>
+                        <!-- </div> -->
                         <!-- End of Chicago -->
-                    </div>
-                </div>
+                    <!-- </div> -->
+                <!-- </div> -->
 
-                <div class="col-lg-6 offset-lg-1">
+                <!-- <div class="col-lg-6 offset-lg-1"> -->
                     <!-- Contact Form -->
-                    <div class="lawFirm--contact-form no-gutters">
+                    <!-- <div class="lawFirm--contact-form no-gutters">
                         <form action="sendmail.php" method="post">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-6"> -->
                                     <!-- Name -->
-                                    <input type="text" name="name" class="form-control" placeholder="Name" required>
-                                </div>
-                                <div class="col-md-6">
+                                    <!-- <input type="text" name="name" class="form-control" placeholder="Name" required>
+                                </div> -->
+                                <!-- <div class="col-md-6"> -->
                                     <!-- Phone Number -->
-                                    <input type="text" name="phone" class="form-control" placeholder="Phone Number" required>
+                                    <!-- <input type="text" name="phone" class="form-control" placeholder="Phone Number" required>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-6"> -->
                                     <!-- Email Address -->
-                                    <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+                                    <!-- <input type="email" name="email" class="form-control" placeholder="Email Address" required>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-6"> -->
                                     <!-- Subject -->
-                                    <input type="text" name="subject" class="form-control" placeholder="Subject" required>
+                                    <!-- <input type="text" name="subject" class="form-control" placeholder="Subject" required>
                                 </div>
-                                <div class="col-md-12">
+                                <div class="col-md-12"> -->
                                     <!-- Message Area -->
-                                    <textarea class="form-control" name="message" placeholder="Message" required></textarea>
+                                    <!-- <textarea class="form-control" name="message" placeholder="Message" required></textarea>
                                 </div>
-                                <div class="col-md-12">
+                                <div class="col-md-12"> -->
                                     <!-- Send Button -->
-                                    <button class="btn" type="submit"> Send Message </button>
+                                    <!-- <button class="btn" type="submit"> Send Message </button>
                                 </div>
                             </div>
-                        </form>
-                        <div class="form-response"></div>
-                    </div>
+                        </form> -->
+                        <!-- <div class="form-response"></div>
+                    </div> -->
                     <!-- End of Contact Form -->
-                </div>
+                <!-- </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- End of Contact -->
 
     <!-- Start Map -->
---
+++ kandidat-1.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("parts/head.php")?>
+<body>
+
+    <!-- Preloader -->
+    <?php include_once("parts/loader.php")?>    
+    <!-- End of Preloader -->
+
+    <!-- Main Header Start -->
+    <?php include_once("parts/header.php")?>
+    <!-- End of Main Header -->
+
+    <!-- 
+
+    <!-- Page title -->
+    <div class="lawFirm--page-title text-center lawFirm--bg-center" data-bg-img="assets/images/page-title-bg.png">
+        <div class="container">
+            <h2 class="pb-20">Lawyer Details</h2>
+            <div class="title-bc"> <a href="#">Home</a> > <span> Lawyer Details </span></div>
+        </div>
+    </div>
+    <!-- End of Page title -->
+
+    <!-- Lawyer Details -->
+    <div class="container pt-120 pb-90">
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+
+                <div class="lawFirm--search-result pb-70">
+                    <div class="row align-items-lg-center align-items-top">
+                        <div class="col-lg-4 col-md-12">
+                            <!-- Lawyer Details Item Image -->
+                            <div class="lawFirm--search-result-image">
+                                <a href="#"><img src="assets/images/result-images/result-1.jpg" alt="" class="img-fluid" data-rjs="2"></a>
+                            </div>
+                            <!-- End of Lawyer Details Item Image -->
+                        </div>
+                        <div class="col-lg-8 col-md-12">
+                            <!-- Lawyer Details Item Data -->
+                            <div class="lawFirm--search-result-text ul-default">
+                                <!-- Lawyer Details Title -->
+                                <h4 class="mb-40">Contact Info</h4>
+
+                                <!-- Lawyer Details Info -->
+                                <ul>
+                                    <li class="d-flex"><span>Name</span> :<i>Andrea Anderson</i></li>
+                                    <li class="d-flex"><span>Practice Area</span> :<i>Corporate Law</i></li>
+                                    <li class="d-flex"><span>Phone</span> :<i>+390280634200</i></li>
+                                    <li class="d-flex"><span>Email</span> :<i>info@yahoomail.com</i></li>
+                                    <li class="d-flex"><span>Office</span>:<i>Milan Piazzetta M. Bossi, 320121 Italy</i></li>
+                                    <li class="d-flex"><span>Follow Me</span> :
+                                        <div class="result-social">
+                                            <a href="#"><i class="fa fa-facebook-f"></i></a>
+                                            <a href="#"><i class="fa fa-twitter"></i></a>
+                                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                                            <a href="#"><i class="fa fa-youtube"></i></a>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <!-- End of Lawyer Details Info -->
+
+                            </div>
+                            <!-- End of Lawyer Details Item Data -->
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Lawyer Details Text -->
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="lawFirm--lawyer-details ul-default mb-20">
+                            <h4>Career and qualifications</h4>
+                            <ul>
+                                <li> Manchester University (LLB, 1st class honours) 1990</li>
+                                <li> Manchester University (LLB, 1st class MBA) 1991 </li>
+                                <li> Joined Clifford Chance 1993 </li>
+                                <li> Admitted as a solicitor in England & Wales 1991</li>
+                                <li> Admitted as a solicitor in England & Wales 1994</li>
+                                <li> Partner since 2000</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-7">
+                        <div class="lawFirm--lawyer-details ul-default mb-20">
+                            <h4>Reputation for acting on ground-breaking, first of a kind mandates</h4>
+                            <p>As a leading practice we work at themarket advising We can offer clients support across the full range of their legal needs, including on:</p>
+                            <ul>
+                                <li>Large cross-border M&A transactions</li>
+                                <li>Complex reorganisation and restructuring</li>
+                                <li>Longevity risk transfers / bulk annuity buy-in</li>
+                                <li>Complex reorganisation and restructuring</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End of Lawyer Details Text -->
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End of Lawyer Details -->
+
+     -->
+
+
+    <!-- Footer -->
+    <?php include_once("parts/footer.php")?>
+    <!-- End of Footer -->
+
+    <!-- Back to Top Button -->
+    <?php include_once("parts/back-to-top.php")?>
+    <!-- End of Back to Top Button -->
+    
+    <!-- JS Files -->
+    <?php include_once("parts/script.php")?>
+    
+
+</body>
+</html>
