eb5eb0fbe8058589f35c0ab57c072035185c61ac
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-07-28 09:42:45 +0700

    Add kalender pilkada and tahapan pilkada page

---
+++ kalender-pilkada.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("parts/head.php")?>
+
+<body>
+
+    <!-- Preloader -->
+    <?php include_once("parts/loader.php")?>
+    <!-- End of Preloader -->
+
+    <!-- Main Header Start -->
+    <?php include_once("parts/header.php")?>
+    <!-- End of Main Header -->
+
+    <!-- Page title -->
+    <div class="lawFirm--page-title text-center lawFirm--bg-center"
+        data-bg-img="assets/images/lokasi-img/header-bg.png">
+        <div class="container">
+            <h2 class="pb-20">Kalender Pilkada 2020</h2>
+            <div class="title-bc"> <a href="#">Home</a> > <span>Kalender</span></div>
+        </div>
+    </div>
+    <!-- End of Page title -->
+
+    <!-- Calendar -->
+    <div class="container pt-120 pb-70">
+        <header>
+            <h4 class="display-4 mb-4 text-center">Agustus 2020</h4>
+            <div class="row d-none d-sm-flex p-1 bg-dark text-white">
+                <h5 class="col-sm p-1 text-center text-white">Minggu</h5>
+                <h5 class="col-sm p-1 text-center text-white">Senin</h5>
+                <h5 class="col-sm p-1 text-center text-white">Selasa</h5>
+                <h5 class="col-sm p-1 text-center text-white">Rabu</h5>
+                <h5 class="col-sm p-1 text-center text-white">Kamis</h5>
+                <h5 class="col-sm p-1 text-center text-white">Jumat</h5>
+                <h5 class="col-sm p-1 text-center text-white">Sabtu</h5>
+            </div>
+        </header>
+        <div class="row border border-right-0 border-bottom-0">
+            <div
+                class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">29</span>
+                    <small class="col d-sm-none text-center text-muted">Minggu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div
+                class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">30</span>
+                    <small class="col d-sm-none text-center text-muted">Senin</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div
+                class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">1</span>
+                    <small class="col d-sm-none text-center text-muted">Selasa</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">2</span>
+                    <small class="col d-sm-none text-center text-muted">Rabu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">3</span>
+                    <small class="col d-sm-none text-center text-muted">Kamis</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">4</span>
+                    <small class="col d-sm-none text-center text-muted">Jumat</small>
+                    <span class="col-1"></span>
+                </h5>
+                <!-- <a class="event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-info text-white"
+                    title="Test Event 1">Test Event 1</a> -->
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">5</span>
+                    <small class="col d-sm-none text-center text-muted">Sabtu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">6</span>
+                    <small class="col d-sm-none text-center text-muted">Minggu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">7</span>
+                    <small class="col d-sm-none text-center text-muted">Senin</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">8</span>
+                    <small class="col d-sm-none text-center text-muted">Selasa</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">9</span>
+                    <small class="col d-sm-none text-center text-muted">Rabu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <!-- <a class="event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-success text-white"
+                    title="Test Event 2">Test Event 2</a> -->
+                <a class="event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-danger text-white"
+                    title="Test Event 3">Pilkada Serentak 2020</a>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">10</span>
+                    <small class="col d-sm-none text-center text-muted">Kamis</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">11</span>
+                    <small class="col d-sm-none text-center text-muted">Jumat</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">12</span>
+                    <small class="col d-sm-none text-center text-muted">Sabtu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">13</span>
+                    <small class="col d-sm-none text-center text-muted">Minggu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">14</span>
+                    <small class="col d-sm-none text-center text-muted">Senin</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">15</span>
+                    <small class="col d-sm-none text-center text-muted">Selasa</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">16</span>
+                    <small class="col d-sm-none text-center text-muted">Rabu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">17</span>
+                    <small class="col d-sm-none text-center text-muted">Kamis</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">18</span>
+                    <small class="col d-sm-none text-center text-muted">Jumat</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">19</span>
+                    <small class="col d-sm-none text-center text-muted">Sabtu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">20</span>
+                    <small class="col d-sm-none text-center text-muted">Minggu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">21</span>
+                    <small class="col d-sm-none text-center text-muted">Senin</small>
+                    <span class="col-1"></span>
+                </h5>
+                <!-- <a class="event d-block p-1 pl-2 pr-2 mb-1 rounded text-truncate small bg-primary text-white"
+                    title="Test Event with Super Duper Really Long Title">Test Event with Super Duper Really Long
+                    Title</a> -->
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">22</span>
+                    <small class="col d-sm-none text-center text-muted">Selasa</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">23</span>
+                    <small class="col d-sm-none text-center text-muted">Rabu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">24</span>
+                    <small class="col d-sm-none text-center text-muted">Kamis</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">25</span>
+                    <small class="col d-sm-none text-center text-muted">Jumat</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">26</span>
+                    <small class="col d-sm-none text-center text-muted">Sabtu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">27</span>
+                    <small class="col d-sm-none text-center text-muted">Minggu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">28</span>
+                    <small class="col d-sm-none text-center text-muted">Senin</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">29</span>
+                    <small class="col d-sm-none text-center text-muted">Selasa</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate ">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">30</span>
+                    <small class="col d-sm-none text-center text-muted">Rabu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">1</span>
+                    <small class="col d-sm-none text-center text-muted">Kamis</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div
+                class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">2</span>
+                    <small class="col d-sm-none text-center text-muted">Jumat</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div
+                class="day col-sm p-2 border border-left-0 border-top-0 text-truncate d-none d-sm-inline-block bg-light text-muted">
+                <h5 class="row align-items-center">
+                    <span class="date col-1">3</span>
+                    <small class="col d-sm-none text-center text-muted">Sabtu</small>
+                    <span class="col-1"></span>
+                </h5>
+                <p class="d-sm-none">No events</p>
+            </div>
+            <div class="w-100"></div>
+            
+        </div>
+    </div>
+    <!-- End of Calendar -->
+
+
+    <!-- Footer -->
+    <?php include_once("parts/footer.php")?>
+    <!-- End of Footer -->
+
+    <!-- Back to Top Button -->
+    <?php include_once("parts/back-to-top.php")?>
+    <!-- End of Back to Top Button -->
+
+    <!-- JS Files -->
+    <?php include_once("parts/script.php")?>
+
+
+</body>
+
+</html>
---
+++ tahapan-pilkada.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("parts/head.php")?>
+
+<body>
+
+    <!-- Preloader -->
+    <?php include_once("parts/loader.php")?>
+    <!-- End of Preloader -->
+
+    <!-- Main Header Start -->
+    <?php include_once("parts/header.php")?>
+    <!-- End of Main Header -->
+
+    <!-- Page title -->
+    <div class="lawFirm--page-title text-center lawFirm--bg-center"
+        data-bg-img="assets/images/lokasi-img/header-bg.png">
+        <div class="container">
+            <h2 class="pb-20">Tahapan Pilkada 2020</h2>
+            <div class="title-bc"> <a href="#">Home</a> > <span>Tahapan Pilkada</span></div>
+        </div>
+    </div>
+    <!-- End of Page title -->
+
+    <!-- Timeline -->
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <h4>Tahapan Pilkada</h4>
+                <ul class="timeline">
+                    <li>
+                        <a href="#" class="float-right">30 Mei 2020 - 31 Januari 2021</a>
+                        <p>Pengaktifan Kembali PPK dan PPS</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">9 Juni - 1 Agustus 2020</a>
+                        <p>Syarat Dukungan Paslon Perseorangan</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">9 Juni - 24 Juni 2020</a>
+                        <p>Ver Faktual</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">4 Juli - 2 Agustus 2020</a>
+                        <p>COKLIT</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">27 Juli - 9 November 2020</a>
+                        <p>Pengadaan LOG</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">9 September - 8 Desember 2020</a>
+                        <p>Produksi dan Distribusi LOG</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">17 Agustus - 8 September 2020</a>
+                        <p>Pengumuman, Pendaftaran, Penelitian, dan Penetapan Paslon</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">8 September - 8 Desember 2020</a>
+                        <p>Sengketa TUN Pemilihan</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">11 September - 5 Desember 2020</a>
+                        <p>Kampanye</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">1 Oktober - 23 November 2020</a>
+                        <p>Pembentukan KPPS</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">24 November - 23 Desember 2020</a>
+                        <p>Masa Kerja KPPS</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">9 Desember 2020</a>
+                        <p class="large font-weight-bold text-danger">Pemungutan Suara</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">9 Desember - 26 Desember 2020</a>
+                        <p>Penghitungan dan Rekapitulasi Suara</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right text-danger">PALING LAMA 5 HARI PASCA PUTUSAN MK</a>
+                        <p>Penetapan Calon Terpilih</p>
+                    </li>
+                    <li>
+                        <a href="#" class="float-right">Mengikuti Jadwal MK</a>
+                        <p>Sengketa</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- End of Timeline -->
+
+
+    <!-- Footer -->
+    <?php include_once("parts/footer.php")?>
+    <!-- End of Footer -->
+
+    <!-- Back to Top Button -->
+    <?php include_once("parts/back-to-top.php")?>
+    <!-- End of Back to Top Button -->
+
+    <!-- JS Files -->
+    <?php include_once("parts/script.php")?>
+
+
+</body>
+
+</html>
