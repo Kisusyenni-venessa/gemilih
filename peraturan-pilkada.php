7350b9e24014d60154c7606586e4bca95034f682
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-07-28 09:41:35 +0700

    Add peraturan pilkada

---
+++ peraturan-pilkada.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("parts/head.php")?>
+
+<body>
+
+    <!-- Preloader -->
+    <?php include_once("parts/loader.php")?>
+    <!-- End of Preloader -->
+
+    <!-- Main Header Start -->
+    <?php include_once("parts/header.php")?>
+    <!-- End of Main Header -->
+
+    <!-- Page title -->
+    <div class="lawFirm--page-title text-center lawFirm--bg-center"
+        data-bg-img="assets/images/lokasi-img/header-bg.png">
+        <div class="container">
+            <h2 class="pb-20">Peraturan Pilkada</h2>
+            <div class="title-bc"> <a href="#">Home</a> > <span>Peraturan Pilkada</span></div>
+        </div>
+    </div>
+    <!-- End of Page title -->
+
+    <!-- Policy -->
+    <div class="container pt-120 pb-90">
+        <div class="lawFirm--practice-area">
+            <h2>Kumpulan Peraturan Tentang Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, serta Walikota dan Wakil Walikota</h2>
+            <div class="row">
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"><a href="#">Peraturan Komisi Pemilihan Umum Nomor 15 Tahun 2019</a></h2>
+                            <p>Tahapan, Program dan Jadwal Penyelenggaraan Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, dan/atau Wali Kota dan Wakil Wali Kota Tahun 2020</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#"> Undang-Undang Republik Indonesia Nomor 10 Tahun 2016</a></h2>
+                            <p>Perubahan Kedua Atas Undang-Undang Nomor 1 Tahun 2015 Tentang Penetapan Peraturan Pemerintah Pengganti Undang-Undang Nomor 1 Tahun 2014 Tentang Pemilihan Gubernur, Bupati, dan Waikota Menjadi Undang-Undang</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">Peraturan Komisi Pemilihan Umum Nomor 15 Tahun 2017</a></h2>
+                            <p>Perubahan atas Peraturan Komisi Pemilihan Umum Nomor 3 Tahun 2017 tentang Pencalonan Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, dan/atau Walikota dan Wakil Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">	Peraturan Komisi Pemilihan Umum Nomor 3 Tahun 2017</a></h2>
+                            <p>Pencalonan Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, dan/atau Walikota dan Wakil Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">KEPPRES Nomor : 25 TAHUN 2015</a> </h2>
+                            <p>Hari Pemungutan Suara Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati serta Walikota dan Wakil Walikota Tahun 2015 Sebagai Hari Libur Nasional</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">	Peraturan Mahkamah Konstitusi Nomor 2 Tahun 2015</a> </h2>
+                            <p>Tahapan, Kegiatan dan Jadwal Penanganan Perkara Perselisihan Hasil Pemilihan Gubernur, Bupati dan Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">Peraturan Mahkamah Konstitusi Nomor 1 Tahun 2015</a> </h2>
+                            <p>Pedoman Beracara Dalam Perkara Perselisihan Hasil Pemilihan Gubernur, Bupati dan Wakil Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">PERMENDAGRI Nomor 51 Tahun 2015</a> </h2>
+                            <p>Perubahan Atas Peraturan Menteri Dalam Negeri Nomor 44 Tahun 2015 Tentang Pengelolaan Dana Kegiatan Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, serta Walikota dan Wakil Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+                <!-- Practice Item -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="lawFirm--practice-area-item mb-30">
+                        <!-- Practice Item Image -->
+                        <div class="lawFirm--practice-area-image">
+                            <a href="#"><img src="assets/images/peraturan/peraturan.jpg" alt="" class="img-fluid"
+                                    data-rjs="2"></a>
+                        </div>
+                        <!-- End of Practice Item Image -->
+
+                        <!-- Practice Item Text -->
+                        <div class="lawFirm--practice-area-text pt-30 pr-20 pb-30 pl-30">
+                            <h2 class="mb-20"> <a href="#">PERMENDAGRI Nomor 44 Tahun 2015</a> </h2>
+                            <p>Pengelolaan Dana Kegiatan Pemilihan Gubernur dan Wakil Gubernur, Bupati dan Wakil Bupati, serta Walikota dan Wakil Walikota</p>
+                        </div>
+                        <!-- End of Practice Item Text -->
+                    </div>
+                </div>
+                <!-- End of Practice Item -->
+
+            </div>
+        </div>
+    </div>
+    <!-- End of Policy -->
+
+
+    <!-- Footer -->
+    <?php include_once("parts/footer.php")?>
+    <!-- End of Footer -->
+
+    <!-- Back to Top Button -->
+    <?php include_once("parts/back-to-top.php")?>
+    <!-- End of Back to Top Button -->
+
+    <!-- JS Files -->
+    <?php include_once("parts/script.php")?>
+
+
+</body>
+
+</html>
